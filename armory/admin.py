047be7ae3f9aedc42eac2719728782bab11765bb
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-06-01 12:49:23 -0400

    Added queries.py for Django II sprint challenge

--- armory/admin.py
+++ armory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Item, Weapon
+
+admin.site.register((Item, Weapon))
 # Register your models here.
+
--- charactercreator/admin.py
+++ charactercreator/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register((Character, Fighter, Mage, Cleric, Thief, Necromancer))
